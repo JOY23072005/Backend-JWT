c3f52b37f85a3e10b24090468acd92d326767534
JOY23072005 <hansjoydeep@gmail.com>
2025-12-06 03:03:19 +0530

    cors enabled

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
+
 import AuthRoutes from "./routes/auth.route.js";
 import UserRoutes from "./routes/user.route.js";
 import { connectDB } from "./lib/db.js";
@@ -7,10 +9,19 @@ import { connectDB } from "./lib/db.js";
 dotenv.config()
 const app = express();
 
+const allowedOrigins = [process.env.FRONTEND_URL]; 
+
+app.use(cors({
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000", "http://127.0.0.1:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
+
 app.use(express.json());
 
 app.get("/",(res,req)=>{
-    res.status(200).json({"message":"This is Test endpoint"})
+    res.status(200).json({message:"This is Test endpoint"})
 })
 app.use("/auth",AuthRoutes);
 app.use("/user",UserRoutes);
