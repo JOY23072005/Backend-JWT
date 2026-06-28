096c2e09d602366cf9f43be4b9dbec6703495f67
JOY23072005 <hansjoydeep@gmail.com>
2026-06-28 12:43:03 +0530

    fix: Route protection added for reward CRUD.

--- src/routes/reward.route.js
+++ src/routes/reward.route.js
@@ -10,34 +10,49 @@ import {
 } from "../controllers/reward.controller.js";
 
 import {protectRoute} from "../middleware/auth.middleware.js";
-
+import {authorize} from "../middleware/authorize.middleware.js"
 import {uploadCSV} from "../middleware/upload.middleware.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getRewards);
 
-router.post("/", protectRoute, createReward);
+router.post(
+  "/",  
+  protectRoute, 
+  authorize('admin','sub-admin'),
+  createReward
+);
 
-router.patch("/:rewardId", protectRoute, updateReward);
+router.patch(
+  "/:rewardId",  
+  protectRoute, 
+  authorize('admin','sub-admin'),
+  updateReward);
 
-router.delete("/:rewardId", protectRoute, deleteReward);
+router.delete("/:rewardId",  
+  protectRoute, 
+  authorize('admin','sub-admin'),
+  deleteReward);
 
 router.patch(
   "/:rewardId/deactivate",
   protectRoute,
+  authorize('admin','sub-admin'),
   deactivateReward
 );
 
 router.patch(
   "/:rewardId/activate",
   protectRoute,
+  authorize('admin','sub-admin'),
   activateReward
 );
 
 router.post(
    "/upload-csv",
    protectRoute,
+   authorize('admin','sub-admin'),
    uploadCSV,
    uploadRewardsCSV
 );
